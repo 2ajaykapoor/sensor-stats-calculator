edb5ff8881a46036d28729ae1a968f6e44d1fa15
2ajaykapoor <2ajaykapoor@gmail.com>
2024-05-12 23:01:23 +0530

    added filter for csv file only

--- src/main/scala/utils/CsvUtil.scala
+++ src/main/scala/utils/CsvUtil.scala
@@ -8,7 +8,7 @@ object CsvUtil {
   def listFiles(directoryPath: String): Iterable[FileInfo] = {
       val directory = new File(directoryPath)
       if (directory.exists() && directory.isDirectory) {
-        val files = directory.listFiles()
+        val files = directory.listFiles().filter(_.getName.endsWith(".csv"))
         files.map(file => FileInfo(s"$directoryPath/${file.getName}"))
       } else {
         throw new RuntimeException("Invalid directory path or directory does not exist.")
